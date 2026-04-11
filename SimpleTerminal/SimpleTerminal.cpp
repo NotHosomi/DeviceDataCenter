74ee1a0700955618fedd541b2cd76152b1ec89b6
Hosomi-PC <surendaotoko@gmail.com>
2026-04-11 02:24:55 +0100

    Add EIS table view

--- SimpleTerminal/SimpleTerminal.cpp
+++ SimpleTerminal/SimpleTerminal.cpp
@@ -9,6 +9,7 @@
 #include "Ingester.h"
 #include "ErrorBarData.h"
 #include "Grapher.h"
+#include "PrintTable.h"
 
 
 int main()
@@ -40,62 +41,46 @@ int main()
 
 		Ingester ingest(devicePath);
 
+		// Load EIS
 		std::cout << "\nFetching EIS values..." << std::endl;
 		std::map<std::string, std::array<double,3>> ImpedanceKeyvals = ingest.GetEisKeyvals();
-		std::cout << "Done" << std::endl;
 
-		std::cout << "Impedances: (100/1000/2000 Hz)" << std::endl;
+		// EIS Table
+		PrintTable EisTable({ "Electrode", "100 Hz", "1000 Hz", "1995 Hz" });
 		double sum = 0.0;
 		int validCount = 0;
 		for (const auto& iter : ImpedanceKeyvals)
 		{
+			std::string colour = TERM_RED;
 			if (iter.second[1] <= 20000)
 			{
 				sum += iter.second[0];
 				validCount += 1;
-				std::cout << TERM_GREEN;
+				colour = TERM_GREEN;
 			}
-			else
-			{
-				std::cout << TERM_RED;
-			}
-			std::cout << "  " << iter.first << ": " << iter.second[0] << " / " << iter.second[1] << " / " << iter.second[2] << TERM_RESET << std::endl;
+			EisTable.AddRow({iter.first, std::to_string(iter.second[0]), std::to_string(iter.second[1]), std::to_string(iter.second[2]) }, colour);
 		}
-		std::cout << "  Average: " << (sum / validCount) << std::endl;
+		EisTable.Print(TERM_YELLOW);
+		std::cout << "Average: " << (sum / validCount) << std::endl;
 
-
-		std::cout << "Plotting EIS..." << std::endl;
+		// EIS Plot
 		std::array<T_ErrorBarD, 2> EisData = ingest.GetEisPlot();
 		grapher.GraphEIS(deviceId, EisData[0], EisData[1]);
-		std::cout << "Done\n" << std::endl;
 
-		std::cout << "Calculating CSCs..." << std::endl;
+		// Load CV
 		std::map<std::string, double> CscVals = ingest.CalculateCscVals();
-		std::cout << "CSC:" << std::endl;
+		PrintTable CscTable({ "Electrode", "CSC (mC/cm^2)" });
 		sum = 0.0;
 		for (const auto& iter : CscVals)
 		{
+			CscTable.AddRow({ iter.first, std::to_string(iter.second) });
 			std::cout << "  " << iter.first << ": " << iter.second << std::endl;
 			sum += iter.second;
 		}
+		CscTable.Print(TERM_YELLOW);
 		std::cout << "  Average: " << (sum / CscVals.size()) << std::endl;
 
-
+		// CIL
+		ingest.GetVoltageTransients()
 	}
-}
-
-void GraphEIS(std::string path, std::string Id)
-{
-	std::cout << "Graphing EIS for " << Id << "..." << std::endl;
-
-}
-
-void GraphCV(std::string path, std::string Id)
-{
-
-}
-
-void GraphCIL(std::string path, std::string Id)
-{
-
 }
